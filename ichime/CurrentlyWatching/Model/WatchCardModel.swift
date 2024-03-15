27e80e1097fae29ca592769627923e5a0c7a507b
Nikita Nafranets <root@dimensi.dev>
2024-03-15 03:49:47 +0300

    fix: watch card date strings

--- ichime/CurrentlyWatching/Model/WatchCardModel.swift
+++ ichime/CurrentlyWatching/Model/WatchCardModel.swift
@@ -8,6 +8,25 @@
 import Foundation
 import ScraperAPI
 
+extension ScraperAPI.Types.WatchShow.UpdateType {
+    var displayName: String {
+        let formatedDate: String
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yy"
+        formatedDate = formatter.string(from: date)
+
+        switch self {
+        case .plan:
+            return String(localized: "В планах с \(formatedDate)")
+        case .release:
+            return String(localized: "Вышло \(formatedDate)")
+        case .update:
+            return String(localized: "Смотрели \(formatedDate)")
+        }
+    }
+}
+
+
 struct WatchCardModel: Equatable, Identifiable, Hashable {
     static func == (lhs: WatchCardModel, rhs: WatchCardModel) -> Bool {
         lhs.id == rhs.id
