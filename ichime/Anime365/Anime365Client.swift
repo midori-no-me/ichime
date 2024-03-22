2f0af9461c2023c967aa0db9aef9751e055e1274
Petr Flaks <petr@flaks.dev>
2024-03-23 02:29:21 +0300

    fix: remove hardcoded date range for ongoings

--- ichime/Anime365/Anime365Client.swift
+++ ichime/Anime365/Anime365Client.swift
@@ -5,8 +5,8 @@
 //  Created by p.flaks on 02.01.2024.
 //
 
-import Foundation
 import Anime365ApiClient
+import Foundation
 
 func convertApiDateStringToDate(string: String, withFormat format: String = "yyyy-MM-dd HH:mm:ss") -> Date? {
     let dateFormatter = DateFormatter()
@@ -39,12 +39,28 @@ class Anime365Client {
         offset: Int,
         limit: Int
     ) async throws -> [Show] {
+        let currentDate = Date()
+        let currentYear = Calendar.current.component(.year, from: currentDate)
+        let currentMonthNumber = Calendar.current.component(.month, from: currentDate)
+
+        let currentAnimeSeason = switch currentMonthNumber {
+        case 12, 1, 2: // December, January, February
+            "winter"
+        case 3, 4, 5: // March, April, May
+            "spring"
+        case 6, 7, 8: // June, July, August
+            "summer"
+        default: // September, October, November
+            "autumn"
+        }
+
         let apiResponse = try await apiClient.sendApiRequest(ListSeriesRequest(
             limit: limit,
             offset: offset,
             chips: [
                 "isAiring": "1",
-                "yearseason": "winter_2023-winter_2024"
+                "isActive": "1",
+                "yearseason": "\(currentAnimeSeason)_\(currentYear - 1)-\(currentAnimeSeason)_\(currentYear)",
             ]
         ))
 
