62e938dac014adbf0f4549fee951c2ddd5d98677
Petr Flaks <petr@flaks.dev>
2024-03-24 15:51:01 +0300

    chore: add AppMetrica

--- ichime/IchimeApp.swift
+++ ichime/IchimeApp.swift
@@ -4,6 +4,7 @@
 //
 //  Created by p.flaks on 01.01.2024.
 //
+import AppMetricaCore
 import DITranquillity
 import ScraperAPI
 import SwiftUI
@@ -11,6 +12,14 @@ import SwiftUI
 @main
 struct IchimeApp: App {
     @Environment(\.scenePhase) private var phase
+
+    init() {
+        // idk it does not compile for tvOS however it's declared as supported platform
+        #if os(iOS)
+            let configuration = AppMetricaConfiguration(apiKey: "f17336dc-d959-4e4e-a103-b2c7a31433d1") // maybe move it somewhere?
+            AppMetrica.activate(with: configuration!)
+        #endif
+    }
 
     var body: some Scene {
         WindowGroup {
