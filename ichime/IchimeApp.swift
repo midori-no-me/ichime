08ae421ee3c6040faeae84960352ead246fb46e5
Petr Flaks <petr@flaks.dev>
2024-03-24 16:26:56 +0300

    build: remove App Metrica because it breaks tvOS

--- ichime/IchimeApp.swift
+++ ichime/IchimeApp.swift
@@ -4,7 +4,6 @@
 //
 //  Created by p.flaks on 01.01.2024.
 //
-import AppMetricaCore
 import DITranquillity
 import ScraperAPI
 import SwiftUI
@@ -12,14 +11,6 @@ import SwiftUI
 @main
 struct IchimeApp: App {
     @Environment(\.scenePhase) private var phase
-
-    init() {
-        // idk it does not compile for tvOS however it's declared as supported platform
-        #if os(iOS)
-            let configuration = AppMetricaConfiguration(apiKey: "f17336dc-d959-4e4e-a103-b2c7a31433d1") // maybe move it somewhere?
-            AppMetrica.activate(with: configuration!)
-        #endif
-    }
 
     var body: some Scene {
         WindowGroup {
