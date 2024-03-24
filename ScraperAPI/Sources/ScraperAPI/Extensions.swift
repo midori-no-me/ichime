7f89b9eda812a72ce41d9f96667a9e63b9cc1e43
Nikita Nafranets <root@dimensi.dev>
2024-03-25 00:03:56 +0300

    feat: write a new moments api (#43)

--- ScraperAPI/Sources/ScraperAPI/Extensions.swift
+++ ScraperAPI/Sources/ScraperAPI/Extensions.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import OSLog
+import SwiftSoup
 
 let logger = Logger(
     subsystem: Bundle.main.infoDictionary?[kCFBundleNameKey as String] as? String ?? "dev.midorinome.ichime",
@@ -37,4 +38,17 @@ func extractIDs(from url: String) -> (showID: Int, episodeID: Int, translationID
     let components = withoutQuery.dropFirst().components(separatedBy: "/").dropFirst()
     let ids = components.map { getId(from: $0) }
     return (showID: ids[0], episodeID: ids[1], translationID: ids.item(at: 2))
+}
+
+
+extension Elements {
+    // background-image: url('/posters/26142.5187294764.140x140.1.jpg'); -> /posters/26142.5187294764.140x140.1.jpg
+    func imageBackground() throws -> String {
+        let styleAttr = try attr("style")
+        guard let matchedText = styleAttr.firstMatch(of: #/url\('(.*)'\);/#)?.output.1 else {
+            return ""
+        }
+
+        return String(matchedText)
+    }
 }
---
+++ ScraperAPI/Sources/ScraperAPI/Request/Moments/GetMomentsByEpisode.swift
@@ -0,0 +1,61 @@
+//
+//  File.swift
+//
+//
+//  Created by Nikita Nafranets on 24.03.2024.
+//
+
+import Foundation
+import SwiftSoup
+
+public extension ScraperAPI.Request {
+    struct GetMomentsByEpisode: ScraperHTMLRequest {
+        public typealias ResponseType = [ScraperAPI.Types.Moment]
+
+        let id: Int
+        let page: Int
+
+        public init(episodeId id: Int, page: Int = 1) {
+            self.id = id
+            self.page = page
+        }
+
+        public func getEndpoint() -> String {
+            "moments/listByEpisode/\(id)"
+        }
+
+        public func getQueryItems() -> [URLQueryItem] {
+            var query: [URLQueryItem] = []
+
+            if page == 1 {
+                query.append(.init(name: "dynpage", value: "1"))
+            } else {
+                query.append(.init(name: "ajaxPage", value: "yw_moments_episodes"))
+                query.append(.init(name: "ajaxPageMode", value: "more"))
+                query.append(.init(name: "moments-page", value: "\(page)"))
+            }
+
+            return query
+        }
+
+        public func getFormData() -> [URLQueryItem] {
+            []
+        }
+
+        public func parseResponse(html: String, baseURL: URL) throws -> [ScraperAPI.Types.Moment] {
+            do {
+                let fragment = try SwiftSoup.parseBodyFragment(html, baseURL.absoluteString)
+
+                return try fragment.select(".m-moment__card")
+                    .map { try ScraperAPI.Types.Moment(from: $0, baseURL: baseURL) }
+
+            } catch {
+                logger
+                    .error(
+                        "\(String(describing: Self.self)): cannot parse html, \(error.localizedDescription, privacy: .public)"
+                    )
+                throw ScraperAPI.APIClientError.parseError
+            }
+        }
+    }
+}
