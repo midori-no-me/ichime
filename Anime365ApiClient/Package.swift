b6251e2638ac478025caeb6c5db29a10213e6a8b
Petr Flaks <petr@flaks.dev>
2024-09-22 00:47:25 +0300

    chore: add xcodegen

--- Anime365ApiClient/Package.swift
+++ Anime365ApiClient/Package.swift
@@ -1,25 +1,26 @@
-// swift-tools-version: 5.9
+// swift-tools-version:6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "Anime365ApiClient",
-    platforms: [
-        .iOS(.v17),
-        .macOS(.v14),
-        .tvOS(.v16)
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Anime365ApiClient",
-            targets: ["Anime365ApiClient"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Anime365ApiClient"),
-    ]
+  name: "Anime365ApiClient",
+  platforms: [
+    .iOS(.v18),
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "Anime365ApiClient",
+      targets: ["Anime365ApiClient"]
+    ),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "Anime365ApiClient"
+    ),
+  ],
+  swiftLanguageModes: [.v5]
 )
