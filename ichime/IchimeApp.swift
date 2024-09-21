d97ef47ea3891a7c7387d7a8fe885530f40413e6
Petr Flaks <petr@flaks.dev>
2024-09-22 00:47:25 +0300

    chore: add xcodegen

--- ichime/IchimeApp.swift
+++ ichime/IchimeApp.swift
@@ -4,7 +4,6 @@
 //
 //  Created by p.flaks on 01.01.2024.
 //
-import AppMetricaCore
 import DITranquillity
 import ScraperAPI
 import SwiftUI
@@ -12,11 +11,6 @@ import SwiftUI
 @main
 struct IchimeApp: App {
     @Environment(\.scenePhase) private var phase
-
-    init() {
-        let configuration = AppMetricaConfiguration(apiKey: "f17336dc-d959-4e4e-a103-b2c7a31433d1")
-        AppMetrica.activate(with: configuration!)
-    }
 
     var body: some Scene {
         WindowGroup {
