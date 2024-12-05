34994b3b6f0b704f0a4614e22c8bc54f1716fbf5
Petr Flaks <petr@flaks.dev>
2024-12-05 23:05:54 +0300

    build: use `.tvOS(.v18)` in Package.swift

--- ShikimoriApiClient/Package.swift
+++ ShikimoriApiClient/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
   name: "ShikimoriApiClient",
   platforms: [
-    .iOS(.v18)
+    .tvOS(.v18)
   ],
   products: [
     // Products define the executables and libraries a package produces, making them visible to other packages.
