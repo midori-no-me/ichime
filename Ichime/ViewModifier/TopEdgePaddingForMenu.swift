f621d42ccc12909245cbef2cb573c970040c7101
Petr Flaks <petr@flaks.dev>
2024-12-07 21:13:47 +0300

    chore: mark `topEdgePaddingForMenu()` for tvOS only

--- Ichime/ViewModifier/TopEdgePaddingForMenu.swift
+++ Ichime/ViewModifier/TopEdgePaddingForMenu.swift
@@ -4,11 +4,11 @@ import SwiftUI
 private struct TopEdgePaddingForMenu: ViewModifier {
   func body(content: Content) -> some View {
     content.padding(.top, 40)
-
   }
 }
 
-/// Consistent padding that prevents menu button from`TabView` to overlap with content. Should be used only on root views used inside `Tab`.
+/// Consistent padding that prevents menu button from`TabView` to overlap with content. Should be used only on root views used inside `Tab`
+@available(tvOS 18.0, *)
 extension View {
   func topEdgePaddingForMenu() -> some View {
     modifier(TopEdgePaddingForMenu())
