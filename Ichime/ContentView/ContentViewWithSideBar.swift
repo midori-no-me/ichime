75a46ca901538e3a3f0da892b290449843379d19
Petr Flaks <petr@flaks.dev>
2025-02-08 17:21:01 +0300

    chore: remove  due to deeplinks not working anyway

--- Ichime/ContentView/ContentViewWithSideBar.swift
+++ Ichime/ContentView/ContentViewWithSideBar.swift
@@ -4,8 +4,6 @@ import SwiftUI
 struct ContentViewWithSideBar: View {
   @AppStorage("ContentViewWithTabView.selectedTab") private var selectedTab: Tabs = .home
 
-  @State private var route: Route?
-
   @State private var viewModel: UserAnimeListCache = ApplicationDependency.container.resolve()
 
   var body: some View {
@@ -67,55 +65,5 @@ struct ContentViewWithSideBar: View {
     .task {
       await self.viewModel.cacheCategories()
     }
-    .sheet(item: self.$route) { route in
-      switch route.type {
-      case .show:
-        NavigationStack {
-          ShowView(showId: route.id)
-        }
-      case .episode:
-        EpisodeTranslationListView(episodeId: route.id)
-      }
-    }
-    .onOpenURL(perform: { url in
-      guard url.scheme == ServiceLocator.topShellSchema,
-        let components = URLComponents(
-          url: url,
-          resolvingAgainstBaseURL: true
-        )
-      else {
-        return
-      }
-
-      guard let action = components.host,
-        let rawId = components.queryItems?.first(where: { $0.name == "id" })?.value,
-        let id = Int(rawId)
-      else {
-        return
-      }
-
-      let episodeTitle = components.queryItems?.first(where: { $0.name == "title" })?.value
-
-      switch action {
-      case URLActions.show.rawValue:
-        print("its show \(id)")
-        self.route = Route(id: id, type: .show)
-      case URLActions.episode.rawValue:
-        print("its episode \(id)")
-        self.route = Route(id: id, type: .episode)
-      default:
-        print("idk")
-      }
-    })
   }
-}
-
-enum URLActions: String {
-  case show
-  case episode
-}
-
-struct Route: Hashable, Identifiable {
-  let id: Int
-  let type: URLActions
 }
--- Ichime/ContentView/ContentViewWithTabBar.swift
+++ Ichime/ContentView/ContentViewWithTabBar.swift
@@ -3,9 +3,6 @@ import SwiftUI
 
 struct ContentViewWithTabBar: View {
   @AppStorage("ContentViewWithTabView.selectedTab") private var selectedTab: Tabs = .home
-  @State private var route: Route?
-
-  @State private var viewModel: UserAnimeListCache = ApplicationDependency.container.resolve()
 
   var body: some View {
     TabView(selection: self.$selectedTab) {
@@ -65,49 +62,5 @@ struct ContentViewWithTabBar: View {
         Image(systemName: "magnifyingglass")
       }
     }
-    .sheet(item: self.$route) { route in
-      switch route.type {
-      case .show:
-        NavigationStack {
-          ShowView(showId: route.id)
-        }
-      case .episode:
-        EpisodeTranslationListView(episodeId: route.id)
-      }
-    }
-    .tabViewStyle(.tabBarOnly)
-    .task {
-      await self.viewModel.cacheCategories()
-    }
-    .onOpenURL(perform: { url in
-      guard url.scheme == ServiceLocator.topShellSchema,
-        let components = URLComponents(
-          url: url,
-          resolvingAgainstBaseURL: true
-        )
-      else {
-        return
-      }
-
-      guard let action = components.host,
-        let rawId = components.queryItems?.first(where: { $0.name == "id" })?.value,
-        let id = Int(rawId)
-      else {
-        return
-      }
-
-      let episodeTitle = components.queryItems?.first(where: { $0.name == "title" })?.value
-
-      switch action {
-      case URLActions.show.rawValue:
-        print("its show \(id)")
-        self.route = Route(id: id, type: .show)
-      case URLActions.episode.rawValue:
-        print("its episode \(id)")
-        self.route = Route(id: id, type: .episode)
-      default:
-        print("idk")
-      }
-    })
   }
 }
