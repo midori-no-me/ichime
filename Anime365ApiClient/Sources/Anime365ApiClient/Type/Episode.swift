73c6c763e9f372095c77bda663e2ba3afaecab96
Petr Flaks <petr@flaks.dev>
2025-02-15 12:59:20 +0300

    fix: some episodes not loaded correctly

--- Anime365ApiClient/Sources/Anime365ApiClient/Type/Episode.swift
+++ Anime365ApiClient/Sources/Anime365ApiClient/Type/Episode.swift
@@ -16,19 +16,9 @@ public struct EpisodeFull: Decodable {
   public let episodeFull: String
   public let episodeInt: String
   public let episodeTitle: String
-  public let episodeType: EpisodeType
   public let firstUploadedDateTime: Date
   public let isActive: Int
   public let isFirstUploaded: Int
   public let seriesId: Int
   public let translations: [Translation]
-}
-
-public enum EpisodeType: String, Decodable {
-  case bd
-  case tv
-  case ona
-  case ova
-  case movie
-  case preview
 }
