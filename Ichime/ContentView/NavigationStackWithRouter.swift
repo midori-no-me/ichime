e028c2ce6e99947941668649ad3d385ae54b2d76
Petr Flaks <petr@flaks.dev>
2025-02-20 12:00:07 +0300

    fix: opening deep links in multiple tabs

--- Ichime/ContentView/NavigationStackWithRouter.swift
+++ Ichime/ContentView/NavigationStackWithRouter.swift
@@ -3,6 +3,14 @@ import SwiftUI
 @MainActor @preconcurrency struct NavigationStackWithRouter<Root>: View where Root: View {
   @State private var path: [Route] = []
 
+  /// Видит ли пользователь текущий NavigationStackWithRouter
+  ///
+  /// В табах у нас по одному стеку на таб. Как только пользователь впервые открывает таб, он остается висеть в памяти.
+  /// Если пользователь пройдется по всем табам, то они все будут висеть в памяти. И во всех табах будут срабатывать
+  /// события типа `.onOpenURL`. С помощью этого стейта мы можем понять, какой именно стек сейчас активен, чтобы
+  /// обрабатывать события только в нем.
+  @State private var isNavigationStackVisible: Bool = false
+
   private let root: () -> Root
 
   @MainActor @preconcurrency init(@ViewBuilder root: @escaping () -> Root) {
@@ -12,6 +20,12 @@ import SwiftUI
   var body: some View {
     NavigationStack(path: self.$path) {
       self.root()
+        .onAppear {
+          self.isNavigationStackVisible = true
+        }
+        .onDisappear {
+          self.isNavigationStackVisible = false
+        }
         .navigationDestination(for: Route.self) { route in
           switch route {
           case let .showByMyAnimeListId(id):
@@ -22,6 +36,10 @@ import SwiftUI
           }
         }
         .onOpenURL { url in
+          if !self.isNavigationStackVisible {
+            return
+          }
+
           let components = URLComponents(string: url.absoluteString)
 
           guard let components else {
