4e1cb26e8f5a24a9069e9265f43d94c5dd27cb55
Petr Flaks <petr@flaks.dev>
2025-08-05 13:20:58 +0300

    fix: remove calendar from MyAnimeList (Jikan)

--- Ichime/Calendar/Model/ShowFromCalendarWithExactReleaseDate.swift
+++ Ichime/Calendar/Model/ShowFromCalendarWithExactReleaseDate.swift
@@ -1,5 +1,4 @@
 import Foundation
-import JikanApiClient
 import ShikimoriApiClient
 
 struct ShowFromCalendarWithExactReleaseDate: Hashable, Identifiable {
@@ -34,72 +33,6 @@ struct ShowFromCalendarWithExactReleaseDate: Hashable, Identifiable {
     self.posterUrl = URL(string: shikimoriBaseUrl.absoluteString + anime.image.original)
     self.nextEpisodeNumber = fromShikimoriCalendarEntry.next_episode
     self.nextEpisodeReleaseDate = fromShikimoriCalendarEntry.next_episode_at
-  }
-
-  init?(
-    fromJikanAnime: JikanApiClient.Anime
-  ) {
-    self.id = fromJikanAnime.mal_id
-    self.title = .init(
-      translated: .init(
-        russian: nil,
-        japaneseRomaji: fromJikanAnime.title
-      )
-    )
-    self.posterUrl = fromJikanAnime.images.jpg.image_url
-
-    guard let dayStringPlural = fromJikanAnime.broadcast.day else {
-      return nil
-    }
-
-    let pluralDayNameToWeekday: [String: Weekday] = [
-      "Mondays": .monday,
-      "Tuesdays": .tuesday,
-      "Wednesdays": .wednesday,
-      "Thursdays": .thursday,
-      "Fridays": .friday,
-      "Saturdays": .saturday,
-      "Sundays": .sunday,
-    ]
-
-    guard let weekday = pluralDayNameToWeekday[dayStringPlural] else {
-      return nil
-    }
-
-    guard let timeString = fromJikanAnime.broadcast.time else {
-      return nil
-    }
-
-    let hourAndMinute = timeString.components(separatedBy: ":")
-
-    guard let hour = Int(hourAndMinute[0]) else {
-      return nil
-    }
-
-    guard let minute = Int(hourAndMinute[1]) else {
-      return nil
-    }
-
-    guard let timezoneIdentifier = fromJikanAnime.broadcast.timezone else {
-      return nil
-    }
-
-    guard let timezone = TimeZone(identifier: timezoneIdentifier) else {
-      return nil
-    }
-
-    guard
-      let nextClosestDate = getNextClosestDate(
-        weekday: weekday,
-        time: (hour: hour, minute: minute),
-        timezone: timezone
-      )
-    else {
-      return nil
-    }
-
-    self.nextEpisodeNumber = nil
-    self.nextEpisodeReleaseDate = nextClosestDate
   }
 
   static func == (lhs: Self, rhs: Self) -> Bool {
