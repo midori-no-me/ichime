7fe5e446824b595a8a4930a7c6815660853e80c1
Petr Flaks <petr@flaks.dev>
2025-11-29 12:50:53 +0300

    chore: remove unused code

--- Ichime/Show/Type/ShowPreviewShikimori.swift
+++ Ichime/Show/Type/ShowPreviewShikimori.swift
@@ -9,44 +9,6 @@ struct ShowPreviewShikimori: Hashable, Identifiable {
   let airingSeason: AiringSeason?
   let kind: ShowKind?
 
-  init(
-    anime: ShikimoriApiClient.AnimePreview,
-    shikimoriBaseUrl: URL
-  ) {
-    self.id = anime.id
-    self.title = .parsed(anime.name, anime.russian != "" ? anime.russian : nil)
-
-    if let image = anime.image {
-      self.posterUrl = URL(string: shikimoriBaseUrl.absoluteString + image.original)
-    }
-    else {
-      self.posterUrl = nil
-    }
-
-    if let scoreString = anime.score, let score = Float(scoreString), score > 0 {
-      self.score = score
-    }
-    else {
-      self.score = nil
-    }
-
-    let dateWithoutTimeFormatter = ShikimoriApiClient.ApiDateDecoder.getDateWithoutTimeFormatter()
-
-    if let airedOnString = anime.aired_on, let airedAt = dateWithoutTimeFormatter.date(from: airedOnString) {
-      self.airingSeason = .init(fromDate: airedAt)
-    }
-    else {
-      self.airingSeason = nil
-    }
-
-    if let kind = anime.kind {
-      self.kind = .create(kind)
-    }
-    else {
-      self.kind = nil
-    }
-  }
-
   init?(
     graphqlAnimePreview: ShikimoriApiClient.GraphQLAnimePreview
   ) {
